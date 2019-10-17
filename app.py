207d6c03a9f2d6fbc5f560e3fa2287ebf8970e9b
Jeremy Brien <brienjl@gmail.com>
2019-10-17 14:52:01 -0700

    created GET and POST methods for stores and items

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask (__name__)
 
-@app.route('/')
-def home():
-    return "hello world!"
+stores = [
+    {
+        'name': 'My wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+# POST /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=['POST']) 
+def create_item(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>') 
+def get_item_in_store(name):
+    pass
 
 app.run(port=5000)
